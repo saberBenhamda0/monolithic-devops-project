4fb50c7f6a80758017fc8e51db53295665719e8c
saberbenhamda <saberbenhamda0@gmail.com>
2026-09-01 14:57:53 +0100

    addedcloutfront to infr

---
+++ terraform/modules/cloudfront/main.tf
@@ -0,0 +1,162 @@
+
+# instade of writing maanuaaly the json for policy we can  use this below method
+/*
+here a reference example 
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::my-bucket/*"
+    }
+  ]
+}
+*/
+# See https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/private-content-restricting-access-to-s3.html
+data "aws_iam_policy_document" "origin_bucket_policy" {
+  statement {
+    sid    = "AllowCloudFrontServicePrincipalReadWrite"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+    ]
+
+    resources = [
+      "${var.s3_arn}/*",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.s3_distribution.arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "b" {
+  bucket = var.s3_bucket
+  policy = data.aws_iam_policy_document.origin_bucket_policy.json
+}
+
+# is like an SA so a private s3 know the identity of cloudfront so it can access the s3
+resource "aws_cloudfront_origin_access_control" "default" {
+  name                              = "default-oac"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+
+  # here we put the config for the source that CF will get attach to it
+  origin {
+    domain_name              = var.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+    origin_id                = var.s3_bucket_id
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Manga2you frontend CDN "
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = var.s3_bucket_id
+
+
+    # this section configure and tell CF what are considere as 2 different requests for cachine
+    forwarded_values {
+        # when this false the request /product?color=red and /product?color=blue are having the same cache served.
+      query_string = false
+
+        # optionally limit to specific ones:
+        #query_string_cache_keys = ["id", "page"]
+
+    # what get forwarded to the origin 
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  # Cache behavior with precedence 0
+  ordered_cache_behavior {
+    path_pattern     = "/content/immutable/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+
+    # CDN can multiple sources for the cache that why we use 
+    # target_origin_id to specify which cache source should be in this cache behavior
+    target_origin_id = var.s3_bucket_id
+
+    forwarded_values {
+      query_string = false
+      headers      = ["Origin"]
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  # Cache behavior with precedence 1
+  ordered_cache_behavior {
+    path_pattern     = "/content/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    # we put the s3 bucket where the frontend is hosted.
+    target_origin_id = var.s3_bucket_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  price_class = "PriceClass_200"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE"]
+    }
+  }
+
+  tags = {
+    Environment = "production"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
---
+++ terraform/modules/cloudfront/variables.tf
@@ -0,0 +1,16 @@
+variable "s3_bucket_id" {
+   type = string
+   description = "s3 bucket id where frontend is hosted"
+}
+
+variable "bucket_regional_domain_name" {
+  type = string
+}
+
+variable "s3_bucket" {
+  type = string
+}
+
+variable "s3_arn" {
+  type = string
+}
